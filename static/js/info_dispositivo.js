bd4e587231edb4a5a2309b72499ed195edb240ce
benjaestupinan <benja.3stupinan@gmail.com>
2024-10-21 13:13:45 -0300

    FEAT :feet: (info_dispositivo): se agrega validacion en front-end y feedback para los comentarios

--- static/js/info_dispositivo.js
+++ static/js/info_dispositivo.js
@@ -13,6 +13,39 @@ const agrandarImg = () => {
     }
 }
 
+const validarNombre = (nombre) => {
+    return nombre && nombre.length >= 3 && nombre.length <= 80
+}
+
+const validarTexto = (texto) => {
+    return texto && texto.length >= 5
+}
+
+let formComment = document.getElementById("formComentario")
+
+formComment.addEventListener("submit", function(event) {
+    event.preventDefault()
+    console.log("submited")
+
+    let divErrores = document.getElementById("erroresComentario")
+
+    let nombre = document.getElementById("autor").value
+    let texto =  document.getElementById("comment").value
+
+    if (!validarNombre(nombre)) {
+        divErrores.innerHTML = "El nombre no cumple con el formato"
+        divErrores.className = "erroresComentario"
+    } else if (!validarTexto(texto)) {
+        divErrores.innerHTML = "El comentario no cumple con el formato"
+        divErrores.className = "erroresComentario"
+    } else {
+        divErrores.innerHTML = ""
+        divErrores.className = ""
+        this.submit()
+    }
+})
+
+
 imagen.addEventListener("click", agrandarImg)
 
 
